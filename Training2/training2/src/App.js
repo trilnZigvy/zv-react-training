37a267801971ae062de177573320c09c0888fa56
Tri Le <90668023+trilnZigvy@users.noreply.github.com>
2021-09-17 10:14:45 +0700

    training2

---
+++ Training2/training2/src/App.js
@@ -0,0 +1,12 @@
+import InputNumber from './component/inputNumber/inputNumber';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <InputNumber/>
+    </div>
+  );
+}
+
+export default App;
